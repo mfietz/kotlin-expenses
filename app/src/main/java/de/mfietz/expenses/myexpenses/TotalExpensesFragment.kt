3afae36bc05b7a028b74c1c08a41cd626c5c408b
Martin Fietz <martin.fietz@gmail.com>
2017-11-19 15:39:37 +0100

    Add total expenses fragment

---
+++ app/src/main/java/de/mfietz/expenses/myexpenses/TotalExpensesFragment.kt
@@ -0,0 +1,25 @@
+package de.mfietz.expenses.myexpenses
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.jetbrains.anko.*
+import org.jetbrains.anko.support.v4.UI
+
+class TotalExpensesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return UI {
+            verticalLayout {
+                padding = dip(16)
+                textView {
+                    text = "Total Expenses:\nNothing yet"
+                    textSize = 18f
+                }
+            }
+        }.view
+    }
+
+}
