7294054eb678daac964a0468f96d14a00269c735
Martin Fietz <martin.fietz@gmail.com>
2017-11-19 17:41:27 +0100

    Replace xml main activity layout with anko component

--- app/src/main/java/de/mfietz/expenses/myexpenses/MainActivity.kt
+++ app/src/main/java/de/mfietz/expenses/myexpenses/MainActivity.kt
@@ -2,16 +2,29 @@ package de.mfietz.expenses.myexpenses
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.FrameLayout
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MainActivityUI().setContentView(this)
         supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, TotalExpensesFragment())
                 .addToBackStack(null)
                 .commit()
+    }
+
+}
+
+class MainActivityUI : AnkoComponent<MainActivity> {
+    override fun createView(ui: AnkoContext<MainActivity>): View = with(ui) {
+        frameLayout {
+            id = R.id.fragment_container
+            layoutParams = FrameLayout.LayoutParams(matchParent, matchParent)
+        }
     }
 
 }
