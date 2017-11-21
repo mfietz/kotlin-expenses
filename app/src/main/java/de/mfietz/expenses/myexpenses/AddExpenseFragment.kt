144395f79fc83b76ff86fb57ff0e2344a5f16f9a
Martin Fietz <martin.fietz@gmail.com>
2017-11-21 13:59:13 +0100

    Add menu item to create new expense categories

--- app/src/main/java/de/mfietz/expenses/myexpenses/AddExpenseFragment.kt
+++ app/src/main/java/de/mfietz/expenses/myexpenses/AddExpenseFragment.kt
@@ -1,11 +1,11 @@
 package de.mfietz.expenses.myexpenses
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.support.v7.app.AlertDialog
+import android.view.*
+import android.widget.*
 import org.jetbrains.anko.customView
 import org.jetbrains.anko.editText
 import org.jetbrains.anko.listView
@@ -15,13 +15,18 @@ import org.jetbrains.anko.support.v4.alert
 import org.jetbrains.anko.support.v4.ctx
 import org.jetbrains.anko.verticalLayout
 
+
 class AddExpenseFragment : Fragment() {
 
     val expenseRepository by lazy { ExpensesRepository(ctx.applicationContext) }
+    val categoriesRepositiry by lazy { CategoriesRepository(ctx.applicationContext) }
+    lateinit var categories: MutableList<String>
+    lateinit var listAdapter: BaseAdapter
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val categories = arrayOf("Food", "Entertainment")
-        val listAdapter = ArrayAdapter(ctx, android.R.layout.simple_list_item_1, categories)
+        setHasOptionsMenu(true)
+        categories = categoriesRepositiry.getAll().toMutableList()
+        listAdapter = ArrayAdapter(ctx, android.R.layout.simple_list_item_1, categories)
         return UI {
             verticalLayout {
                 listView {
@@ -33,6 +38,32 @@ class AddExpenseFragment : Fragment() {
                 }
             }
         }.view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.categories_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        val itemId = item?.itemId
+        if(itemId == R.id.action_add_category) {
+            val input = EditText(ctx)
+            input.layoutParams = LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT)
+            AlertDialog.Builder(ctx)
+                    .setView(input)
+                    .setPositiveButton(android.R.string.ok, { dialogInterface, id ->
+                        val newCategory = input.text.toString()
+                        categoriesRepositiry.add(newCategory)
+                        categories.add(newCategory)
+                        listAdapter.notifyDataSetChanged()
+                    })
+                    .show()
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun askAmount(category: String) {
---
+++ app/src/main/java/de/mfietz/expenses/myexpenses/CategoriesRepository.kt
@@ -0,0 +1,28 @@
+package de.mfietz.expenses.myexpenses
+
+import android.content.Context
+import com.google.gson.Gson
+import org.jetbrains.anko.defaultSharedPreferences
+
+class CategoriesRepository(ctx: Context) {
+
+    val preferences = ctx.defaultSharedPreferences
+    val gson = Gson()
+
+    private companion object {
+        val PREFERENCE_KEY = "categories"
+    }
+
+    fun add(category: String) {
+        val expenses = getAll().toMutableSet()
+        expenses.add(category)
+        preferences.edit()
+                .putStringSet(PREFERENCE_KEY, expenses)
+                .apply()
+    }
+
+    fun getAll(): Set<String> {
+        return preferences.getStringSet(PREFERENCE_KEY, emptySet())
+    }
+
+}
